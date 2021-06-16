246f372ca4a89de45eeea4259ae2e015ebf3eca0
Ismael Fernandez <ishy360@gmail.com>
2021-06-15 20:42:33 -0500

    Adding sonos control features

--- WelcomeHome/WelcomeHome.Console/Program.cs
+++ WelcomeHome/WelcomeHome.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ByteDev.Sonos;
+using ByteDev.Sonos.Models;
+using ByteDev.Sonos.Upnp.Services.Models;
+using System;
 using WemoNet;
 
 namespace WelcomeHome.Console
@@ -9,6 +12,8 @@ namespace WelcomeHome.Console
         {
             ConsoleKeyInfo input;
             Wemo wemo = new Wemo();
+            SonosController sonosController = new SonosControllerFactory().Create("10.0.0.5");
+
             do
             {
                 input = System.Console.ReadKey();
@@ -16,10 +21,16 @@ namespace WelcomeHome.Console
                 if (Char.ToLower(input.KeyChar) == 'h')
                 {
                     wemo.TurnOnWemoPlugAsync("http://10.0.0.9").GetAwaiter().GetResult();
+                    sonosController.ClearQueueAsync();
+                    sonosController.SetPlayModeAsync(new PlayMode(PlayModeType.RepeatOne));
+                    sonosController.AddQueueTrackAsync(trackUri: "x-file-cifs://10.0.0.17/music/Noises/test.mp3", enqueueAsNext: true).Wait();
+                    sonosController.SetVolumeAsync(new SonosVolume(70));
+                    sonosController.PlayAsync();
                 }
                 else if (Char.ToLower(input.KeyChar) == 'l')
                 {
                     wemo.TurnOffWemoPlugAsync("http://10.0.0.9").GetAwaiter().GetResult();
+                    sonosController.ClearQueueAsync();
                 }
             }
             while (Char.ToLower(input.KeyChar) != 'e');
