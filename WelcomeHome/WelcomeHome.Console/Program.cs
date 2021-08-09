2bb4102e1583e52b916b5bfc416729164ab61d82
Ismael Fernandez <ishy360@gmail.com>
2021-08-08 19:02:37 -0500

    Able to pass simple on off params via command line

--- WelcomeHome/WelcomeHome.Console/Program.cs
+++ WelcomeHome/WelcomeHome.Console/Program.cs
@@ -1,39 +1,43 @@
 ﻿using ByteDev.Sonos;
 using ByteDev.Sonos.Models;
 using ByteDev.Sonos.Upnp.Services.Models;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Hosting;
 using System;
+using System.Threading.Tasks;
 using WemoNet;
 
 namespace WelcomeHome.Console
 {
-    class Program
+    class Program : ConsoleAppBase
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            ConsoleKeyInfo input;
+            await Host.CreateDefaultBuilder().RunConsoleAppFrameworkAsync<Program>(args);
+        }
+
+        [Command("on")]
+        public void on()
+        {
+            Wemo wemo = new Wemo();
+            SonosController sonosController = new SonosControllerFactory().Create("10.0.0.2");
+
+            wemo.TurnOnWemoPlugAsync("http://10.0.0.9").GetAwaiter().GetResult();
+            sonosController.ClearQueueAsync();
+            sonosController.SetPlayModeAsync(new PlayMode(PlayModeType.RepeatOne));
+            sonosController.AddQueueTrackAsync(trackUri: "x-file-cifs://10.0.0.17/music/Noises/test.mp3", enqueueAsNext: true).Wait();
+            sonosController.SetVolumeAsync(new SonosVolume(70));
+            sonosController.PlayAsync();
+        }
+
+        [Command("off")]
+        public void off()
+        {
             Wemo wemo = new Wemo();
             SonosController sonosController = new SonosControllerFactory().Create("10.0.0.5");
 
-            do
-            {
-                input = System.Console.ReadKey();
-
-                if (Char.ToLower(input.KeyChar) == 'h')
-                {
-                    wemo.TurnOnWemoPlugAsync("http://10.0.0.9").GetAwaiter().GetResult();
-                    sonosController.ClearQueueAsync();
-                    sonosController.SetPlayModeAsync(new PlayMode(PlayModeType.RepeatOne));
-                    sonosController.AddQueueTrackAsync(trackUri: "x-file-cifs://10.0.0.17/music/Noises/test.mp3", enqueueAsNext: true).Wait();
-                    sonosController.SetVolumeAsync(new SonosVolume(70));
-                    sonosController.PlayAsync();
-                }
-                else if (Char.ToLower(input.KeyChar) == 'l')
-                {
-                    wemo.TurnOffWemoPlugAsync("http://10.0.0.9").GetAwaiter().GetResult();
-                    sonosController.ClearQueueAsync();
-                }
-            }
-            while (Char.ToLower(input.KeyChar) != 'e');
+            wemo.TurnOffWemoPlugAsync("http://10.0.0.9").GetAwaiter().GetResult();
+            sonosController.ClearQueueAsync();
         }
     }
 }
